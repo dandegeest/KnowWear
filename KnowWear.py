2ff8f475e7b15b6ea48b8dce171814fc8446c95d
DD <73483425+dandegeest@users.noreply.github.com>
2024-12-06 14:09:40 -0600

    Updates for Final
    Add logo, clothing images, and worked on layout

--- KnowWear.py
+++ KnowWear.py
@@ -1,12 +1,13 @@
 import streamlit as st
+from PIL import Image
 import requests
+import random
 from datetime import datetime
 from http import HTTPStatus
 
+st.set_page_config("KnowWear", "assets/logo.png")
 location = "50014"
-# Title for the Streamlit app
-st.title("KnowWear")
-st.subheader("When to Wear What")
+username = "Thomas"
 
 def tomorrow_io(url):
     try:
@@ -20,7 +21,7 @@ def tomorrow_io(url):
         if response.status_code == HTTPStatus.UNAUTHORIZED:
             st.error("Error: Unauthorized. Check your API key.")
         elif response.status_code == HTTPStatus.TOO_MANY_REQUESTS:
-            st.warning("Error: Rate limit exceeded. Try again later.")
+            col2.warning("Error: Rate limit exceeded. Try again later.")
         else:
             st.error(f"Error: Unexpected status code {response.status_code}")
     except requests.exceptions.RequestException as e:
@@ -62,6 +63,12 @@ def get_weather_icon(weather_code):
     icon_url = weather_code_to_icon.get(wc, "http://openweathermap.org/img/wn/01d@2x.png")
     return icon_url
 
+# Title for the Streamlit app
+st.image("assets/logo.png", width=208)
+col1, col2 = st.columns(2)
+with col1:
+    st.write("Hi Thomas! \n It is going to be cold today but looks like it will be mostly sunny and dry.  Here are some outfits that will be perfect for you today. Wear your winter coat, hat, and gloves!")
+
 data = get_current_weather()
 
 if data:
@@ -77,7 +84,7 @@ if data:
     # HTML formatted output
     html_output = f"""
     <div style='font-family: Arial; color: #333;'>
-        <h2 style='color: #0059b3;'>Current Weather Conditions for {location}<img src='{get_weather_icon(weather_code)}'/></h2>
+        <h2 style='color: #0059b3;'>Current Weather: {location}<img src='{get_weather_icon(weather_code)}'/></h2>
         <p><strong>Temperature:</strong> {temperature}°F</p>
         <p><strong>Humidity:</strong> {humidity}%</p>
         <p><strong>Wind Speed:</strong> {wind_speed} mph</p>
@@ -87,7 +94,17 @@ if data:
     """
 
     # Display the HTML in Streamlit
-    st.markdown(html_output, unsafe_allow_html=True)
+    with col2:
+        st.markdown(html_output, unsafe_allow_html=True)
     #st.json(data)
 else:
-    st.warning("Unable to fetch weather data at the moment.")
+    col2.warning("Unable to fetch weather data at the moment.")
+
+with col1:
+    randO = random.randint(1,2)
+    w1, w2, w3, w4 = st.columns(4)
+    w1.image(f"assets/hood{randO}.jpeg", width = 90)
+    w2.image(f"assets/shirt{randO}.jpeg", width = 90)
+    w3.image(f"assets/pants{randO}.jpeg", width = 90)
+    w4.image(f"assets/shoes{randO}.jpeg", width = 90)
+    w4.button("Next")
